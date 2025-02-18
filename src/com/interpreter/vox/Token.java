f3d83a768b3f38f195b3994e5060fc8e896fce3f
panzatros <ragde.15.black@gmail.com>
2025-02-17 18:35:58 -0600

    first part of the project, this is based on the book crafting compilers

---
+++ src/com/interpreter/vox/Token.java
@@ -0,0 +1,30 @@
+package com.interpreter.vox;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
+public record Token( TokenType type, String lexeme, Object literal, int line ) {
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
